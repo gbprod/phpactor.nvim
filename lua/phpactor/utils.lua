72664a24b0ce4a187ab859c1209f03c7ad7df80d
gbprod <contact@gb-prod.fr>
2024-01-18 20:03:26 +0100

    feat: introduce copy_fcqn command
    Fixes: #4

--- lua/phpactor/utils.lua
+++ lua/phpactor/utils.lua
@@ -88,4 +88,23 @@ function utils.get_open_cmd_form_target(target)
   return target
 end
 
+function utils.get_default_register()
+  local clipboard_tool = vim.fn["provider#clipboard#Executable"]()
+  if not clipboard_tool or "" == clipboard_tool then
+    return '"'
+  end
+
+  local clipboard_flags = vim.split(vim.api.nvim_get_option("clipboard"), ",")
+
+  if vim.tbl_contains(clipboard_flags, "unnamedplus") then
+    return "+"
+  end
+
+  if vim.tbl_contains(clipboard_flags, "unnamed") then
+    return "*"
+  end
+
+  return '"'
+end
+
 return utils
