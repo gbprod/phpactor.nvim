efaa0130055e20bcb42acb371817987c8171f477
gbprod <contact@gb-prod.fr>
2024-04-02 10:27:26 +0200

    fix: use pairs instead of ipairs
    Fixes: #34

--- lua/phpactor/handler/lsp/debug/config.lua
+++ lua/phpactor/handler/lsp/debug/config.lua
@@ -4,7 +4,7 @@ return function()
   local results, _ = vim.lsp.buf_request_sync(0, "phpactor/debug/config", { ["return"] = true })
 
   local message = {}
-  for _, result in ipairs(results or {}) do
+  for _, result in pairs(results or {}) do
     message = vim.tbl_extend("force", message, vim.split(result["result"] or {}, "\n", {}))
   end
 
